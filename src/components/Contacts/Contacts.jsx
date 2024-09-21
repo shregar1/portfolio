2f69ef36e3081038f5e49f5b6fb1536a831043dc
Shreyansh Singh Sengar <sengarsinghshreyansh@gmail.com>
2024-09-21 11:07:31 +0530

    bug fix

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -17,9 +17,9 @@ const Contacts = () => {
     }
   };
   return (
-    <div className="flex flex-col flex-wrap justify-center items-center">
+    <div className="flex flex-col flex-wrap justify-center items-center py-10">
       <p className="text-2xl text-white font-mono py-5">CONTACTS</p>
-      <p className="contact-title text-7xl md:text-6xl md:w-1/2 font-mono text-center w-full">
+      <p className="contact-title text-7xl md:text-6xl md:w-1/2 font-mono text-center w-full py-20">
         TELL ME ABOUT YOUR NEXT PROJECT
       </p>
       <button
